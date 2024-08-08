8cb21a6134565b411a29b3a56021b766954dc96f
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-08-08 14:00:03 +0200

    Setting up the sidebar and its main components.

---
+++ src/app/dashboard/dashboardWrapper.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DashboardWrapper = ({ children }: {children: React.ReactNode} ) => {
+  return (
+    <div>DashboardWrapper {children}</div>
+  )
+}
+
+export default DashboardWrapper
