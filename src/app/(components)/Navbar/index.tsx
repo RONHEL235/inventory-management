0156390c7ee7c41411104339fcc52fcb56faed24
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-08-08 16:26:12 +0200

    Completed simple outline of top navigation, yet to implement functionality.

--- src/app/(components)/Navbar/index.tsx
+++ src/app/(components)/Navbar/index.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-import { Bell, Menu, Sun } from 'lucide-react'
+import { Bell, Menu, Settings, Sun } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
@@ -11,7 +12,6 @@ const Navbar = () => {
             <button className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100" onClick={() => {}}>
                 <Menu className="w-4 h-4" />
             </button>
-        </div>
         <div className="relative">
             <input type="search" 
             placeholder="Start type to search groups & products" className="pl-10 pr-4 py-2 w-50 mb:w-80 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500" />
@@ -19,6 +19,7 @@ const Navbar = () => {
                 <Bell className="text-gray-500" size={20} />
             </div>
         </div>
+    </div>
 
         {/*RIGHT SIDE*/}
 
@@ -30,12 +31,24 @@ const Navbar = () => {
                     </button>
                 </div>
                 <div className="relative">
-                    <Bell className="cursor-pointer textt-gray-500" size={24} />
-                    <span className="absolute -top">
-
+                    <Bell className="cursor-pointer text-gray-500" size={24} />
+                    <span className="absolute -top-2 -right-2 inline-flex items-center justify-center px-[0.4rem] py-1 text-xs font-semibold leading-none text-red-100 bg-red-400 rounded-full">
+                        3
+                    </span>
+                </div>
+                <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3"/>
+                <div className="flex items-center gap-3 cursor-pointer">
+                    <div className="w-9 h-9">
+                        image
+                    </div>
+                    <span className="font-semibold">
+                        Ronee Helepi 
                     </span>
                 </div>
             </div>
+            <Link href="/settings">
+                <Settings className="cursor-pointer text-gray-500" size={24} />
+            </Link>
         </div>
     </div>
   )
