c72d3c5021750623c80211442408b44673bfb90a
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-08-11 16:34:50 +0200

    Implemented state for toggling of sidebar on small screens.

--- src/app/(components)/Navbar/index.tsx
+++ src/app/(components)/Navbar/index.tsx
@@ -1,15 +1,24 @@
 "use client"
 
+import { useAppDispatch, useAppSelector } from '@/app/redux'
+import { setIsSidebarCollapsed } from '@/state'
 import { Bell, Menu, Settings, Sun } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
+    const dispatch = useAppDispatch()
+    const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed) 
+  
+    const toggleSidebar = () => {
+      dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))
+    }
+
   return (
     <div className="flex justify-between items-center w-full mb-7">
         {/*LEFT SIDE*/}
         <div className="flex justify-between items-center gap-5">
-            <button className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100" onClick={() => {}}>
+            <button className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100" onClick={toggleSidebar}>
                 <Menu className="w-4 h-4" />
             </button>
         <div className="relative">
--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -1,16 +1,31 @@
 "use client"
 
+import { useAppDispatch, useAppSelector } from '@/app/redux'
+import { setIsSidebarCollapsed } from '@/state'
 import { Menu } from 'lucide-react'
 import React from 'react'
 
 const Sidebar = () => {
+
+  const dispatch = useAppDispatch()
+  const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed) 
+
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))
+  }
+
+  const sidebarClassNames = `fixed flex flex-col ${
+    isSidebarCollapsed ? "w-0 md:w-16" : "w-72 md:w-64"
+  } bg-white transition-all duration 300 overflow-hidden h-full shadow-md z-40`
+
   return (
-    <div>
+    <div className={sidebarClassNames}>
       {/* TOP LOGO */}
       <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
         <div>logo</div>
         <h1 className="font-extrabold text-2xl">RonStock</h1>
-        <button className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100" onClick={() => {}}>
+        <button className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100" 
+        onClick={toggleSidebar}>
           <Menu className="w-4 h-4" />
         </button>
       </div> 
