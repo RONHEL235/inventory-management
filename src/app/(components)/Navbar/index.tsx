4e94333c244f52539329fa0a6030d310c6151684
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-08-24 07:02:35 +0200

    Spacing modifications of the navbar.

--- src/app/(components)/Navbar/index.tsx
+++ src/app/(components)/Navbar/index.tsx
@@ -29,7 +29,7 @@ const Navbar = () => {
             </button>
         <div className="relative">
             <input type="search" 
-            placeholder="Start type to search groups & products" className="pl-10 pr-4 py-2 w-50 mb:w-80 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500" />
+            placeholder="Start type to search groups & products" className="pl-10 pr-4 py-2 w-50 mb:w-60 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500" />
             <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-non">
                 <Bell className="text-gray-500" size={20} />
             </div>
