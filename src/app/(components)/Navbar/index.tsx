c94883515c5df3356367ffb91175bc3acd45b1f0
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-08-24 06:53:02 +0200

    Implemented dark mode.

--- src/app/(components)/Navbar/index.tsx
+++ src/app/(components)/Navbar/index.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useAppDispatch, useAppSelector } from '@/app/redux'
-import { setIsSidebarCollapsed } from '@/state'
+import { setIsDarkMode, setIsSidebarCollapsed } from '@/state'
 import { Bell, Menu, Settings, Sun } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
@@ -9,10 +9,16 @@ import React from 'react'
 const Navbar = () => {
     const dispatch = useAppDispatch()
     const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed) 
+
+    const isDarkMode = useAppSelector((state) => state.global.isDarkMode)
   
     const toggleSidebar = () => {
       dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))
     }
+
+    const toggleDarkMode = () => {
+        dispatch(setIsDarkMode(!isDarkMode))
+    } 
 
   return (
     <div className="flex justify-between items-center w-full mb-7">
@@ -35,7 +41,7 @@ const Navbar = () => {
         <div className="flex justify-between items-center gap-5">
             <div className="hidden md:flex justify-between items-center gap-5">
                 <div>
-                    <button onClick={() => {}}>
+                    <button onClick={toggleDarkMode}>
                         <Sun className="cursor-pointer text-gray-500" size={24} />
                     </button>
                 </div>
--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -105,8 +105,10 @@ const Sidebar = () => {
         />
       </div>
       {/* FOOTER */}
-      <div>
-        <p className="text-center text-xs text-gray-500">&copy; 2027 RonStock</p>
+      <div className={`${isSidebarCollapsed ? "hidden" : "block"} mb-10`}>
+        <p className="text-center text-xs text-gray-500">
+          &copy; 2027 RonStock
+        </p>
       </div>
     </div>
   )
