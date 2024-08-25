3ae28c3702feeb23094624bee75a603d1955d44e
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-08-25 09:56:56 +0200

    Cleaning the codebase.

--- src/app/(components)/Navbar/index.tsx
+++ src/app/(components)/Navbar/index.tsx
@@ -55,7 +55,9 @@ const Navbar = () => {
                     </button>
                 </div>
                 <div className="relative">
-                    <Bell className="cursor-pointer text-gray-500" size={24} />
+                    <Bell 
+                    className="cursor-pointer text-gray-500" 
+                    size={24} />
                     <span className="absolute -top-2 -right-2 inline-flex items-center justify-center px-[0.4rem] py-1 text-xs font-semibold leading-none text-red-100 bg-red-400 rounded-full">
                         3
                     </span>
@@ -71,7 +73,9 @@ const Navbar = () => {
                 </div>
             </div>
             <Link href="/settings">
-                <Settings className="cursor-pointer text-gray-500" size={24} />
+                <Settings 
+                className="cursor-pointer text-gray-500" 
+                size={24} />
             </Link>
         </div>
     </div>
