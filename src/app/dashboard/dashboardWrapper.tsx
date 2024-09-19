f861bbd3feb51abf1f70c8b5f69260e1e9c33d9b
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-09-19 18:54:23 +0200

    Dashboard changes.

--- src/app/dashboard/dashboardWrapper.tsx
+++ src/app/dashboard/dashboardWrapper.tsx
@@ -21,7 +21,7 @@ const isDarkMode = useAppSelector((state) => state.global.isDarkMode)
   return (
     <div className={`${isDarkMode ? "dark" : "light"} flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
         <Sidebar /> 
-        <main className={`flex flex-col w-full h-full py-7 px-10 bg-gray-50 ${isSidebarCollapsed? "md:pl-24" : "md:pl-72" }`}>
+        <main className={`flex flex-col w-full h-full py-7 px-10 bg-gray-50 ${isSidebarCollapsed? "md:pl-20" : "md:pl-72" }`}>
             <Navbar /> 
             {children}
         </main>
