82082f5ff1d474d3df76ac4fd4c42dbd29ae5796
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-10-08 19:40:12 +0200

    Comment: Fixing the backend

--- server/src/routes/dashboardRoutes.ts
+++ server/src/routes/dashboardRoutes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express"
 import { getDashboardMetrics } from "../controllers/dashboardControllers"
 
+//Sort out connecting the frontend to the backend
+
 const router = Router()
 
 router.get("/", getDashboardMetrics)
